5d03bea7f42420275e615cf075d3f7902335edca
gustavo <67645041g@gmail.com>
2025-07-03 15:30:26 -0400

    ultimos cambios de comit espero que funcione

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, Request, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "src/auth/guard/auth.guard";
 import { TaskService } from "./task.service";
 import { CreateTaskDto } from "./dto/create-task.dto";
+import { UpdateTaskDto } from "./dto/update-task.dto";
 
 @Controller('task')
 @UseGuards(AuthGuard)
@@ -17,9 +18,38 @@ export class TaskController {
 
 
     @Get()
-    async getTasks (@Request() req){
+    async getTasks(@Request() req) {
         const userId = req.user.id;
         return this.taskService.getAlltask(userId)
+    }
+
+
+    @Get(':id')
+    async getTask(@Request() req, @Param('id') id: string) {
+        const userId = req.user.id;
+        const taskId = parseInt(id);
+        return this.taskService.getTaskService(userId, taskId)
+    }
+
+
+    //borra la tarea
+
+    @Delete(':id')
+    async deleteTask(@Request() req, @Param('id') id: string) {
+        const userId = req.user.id;
+        const taskId = parseInt(id);
+        return this.taskService.deleteTask(userId, taskId)
+
+    }
+
+    @Put(':id')
+    async updateTask(@Request() req,
+        @Param('id') id: string,
+        @Body() updateData: UpdateTaskDto) {
+        const userId = req.user.id;
+        const taskId = parseInt(id);
+        return this.taskService.updateTask(userId, taskId,updateData)
+
     }
 
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { JwtService } from '@nestjs/jwt';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TaskService {
@@ -13,9 +14,9 @@ export class TaskService {
     async getAlltask(userId: number) {
         try {
             //console.log('userId recibido en getAlltask:', userId);
-            const tasks =await this.prismaService.task.findMany({
-                where:{
-                    userId:userId
+            const tasks = await this.prismaService.task.findMany({
+                where: {
+                    userId: userId
                 }
             });
 
@@ -65,11 +66,86 @@ export class TaskService {
         }
     }
 
-    updateTask() {
-        return 'aqui se modifico una tarea'
+
+    //obtendremos solo una tarea
+
+    async getTaskService(userId: number, taskId: number) {
+        try {
+            const task = await this.prismaService.task.findFirst({
+                where: {
+                    id: taskId,
+                    userId: userId
+                }
+            });
+
+            if (!task) {
+                throw new BadRequestException('Tarea no encontrada');
+            }
+
+            return task;
+
+        } catch (error) {
+            if (error instanceof BadRequestException) {
+                throw error
+            }
+            throw new Error(error)
+        }
     }
 
-    deleteTask() {
-        return 'se borro la tarea'
+
+    //borra todo lo de tareas
+    async deleteTask(userId: number, taskId: number) {
+        try {
+            const tasksFound = await this.prismaService.task.findFirst({
+                where: {
+                    id: taskId,
+                    userId: userId
+                }
+            })
+
+            if (!tasksFound) throw new BadRequestException('la tarea no existe');
+
+            const taskDelete = await this.prismaService.task.delete({
+                where: {
+                    id: taskId
+                }
+            });
+
+            return taskDelete;
+
+        } catch (error) {
+            if (error instanceof BadRequestException) {
+                throw error
+            }
+            throw new Error(error)
+        }
+    }
+
+    async updateTask(userId: number, taskId: number, data: UpdateTaskDto) {
+        try {
+
+            const task = await this.prismaService.task.findFirst({
+                where: {
+                    id: taskId,
+                    userId: userId,
+                },
+            });
+
+            if (!task) {
+                throw new BadRequestException('Tarea no encontrada');
+            }
+
+            const updatedTask = await this.prismaService.task.update({
+                where: { id: taskId },
+                data,
+            });
+
+            return updatedTask;
+        } catch (error) {
+            if (error instanceof BadRequestException) {
+                throw error
+            }
+            throw new Error(error)
+        }
     }
 }
