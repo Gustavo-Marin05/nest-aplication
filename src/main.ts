458b60379fb65a985a1e48233710574e2c6e1e60
gustavo <67645041g@gmail.com>
2025-07-05 10:08:49 -0400

    comit cambio de la url del lado del frontend

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
 
   app.enableCors({
-    origin: true, // Cambia esto al URL de tu frontend
+    origin: 'http://localhost:5173', // Cambia esto al URL de tu frontend
     credentials: true,               // Permite enviar cookies en peticiones
   });
 
