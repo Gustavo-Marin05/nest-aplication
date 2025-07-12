5709cefee6db67b6c5c0ac532b1c891d36ffc241
gustavo <67645041g@gmail.com>
2025-07-12 17:56:11 -0400

    seeee

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import * as express from 'express';
-import { join } from 'path';
-import { Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,21 +8,10 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: 'https://mitarea-0rtj.onrender.com',
+    origin: 'https://mitarea-0rtj.onrender.com', // URL de tu frontend en Render
     credentials: true,
   });
 
-  // Accede a la instancia subyacente de Express
-  const expressApp = app.getHttpAdapter().getInstance();
-
-  // Servir los archivos estáticos de React
-  expressApp.use(express.static(join(__dirname, '..', 'dist')));
-
-  // Redirigir cualquier otra ruta al index.html de React
-  expressApp.get('*', (req: Request, res: Response) => {
-    res.sendFile(join(__dirname, '..', 'dist', 'index.html'));
-  });
-
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
